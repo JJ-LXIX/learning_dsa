377535d09dd3e23fbd802a406541273c319720bf
Jagrath Shetty <jagrathshettylxix@gmail.com>
2024-03-19 11:48:14 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+import binarySearch from "./algorithms/binarySearch";
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
