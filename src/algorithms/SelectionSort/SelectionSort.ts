80a8ebbf7409844b4ecf2f09c1b97a86f0f5085b
Jagrath Shetty <jagrathshettylxix@gmail.com>
2024-03-20 11:39:16 +0530

    added selection sort algo

---
+++ src/algorithms/SelectionSort/SelectionSort.ts
@@ -0,0 +1,16 @@
+const selectionSort = (arr: number[]) => {
+  let n = arr.length;
+  for (let i = 0; i < n - 1; i++) {
+    let min_index = i;
+
+    for (let j = i + 1; j < n; j++) {
+      if (arr[j] < arr[min_index]) {
+        min_index = j;
+      }
+    }
+    [arr[i], arr[min_index]] = [arr[min_index], arr[i]];
+  }
+  return arr;
+};
+
+export default selectionSort;
--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,16 @@ import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 
 import binarySearch from "./algorithms/binarySearch/binarySearch";
+import selectionSort from "./algorithms/SelectionSort/SelectionSort";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-const arr = [1, 3, 5, 6, 7, 8, 9, 13, 14, 15];
+// const arr = [1, 3, 5, 6, 7, 8, 9, 13, 14, 15];
+const arr = [7, 1, 5, 3, 14, 6, 15, 13, 8, 9];
 
-console.log(binarySearch(arr, 3));
+console.log(selectionSort(arr));
 
 app.listen(port);
