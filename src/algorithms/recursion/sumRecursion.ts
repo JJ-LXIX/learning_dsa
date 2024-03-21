27c1df511a8f19ce763822755970f6944379c6dc
Jagrath Shetty <jagrathshettylxix@gmail.com>
2024-03-21 11:34:00 +0530

    added sum of array using recursion

---
+++ src/algorithms/recursion/sumRecursion.ts
@@ -0,0 +1,5 @@
+const sumRecursion = (arr: number[]): number => {
+  return arr.length === 0 ? 0 : arr[0] + sumRecursion(arr.slice(1));
+};
+
+export default sumRecursion;
--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,16 @@ import dotenv from "dotenv";
 
 import binarySearch from "./algorithms/binarySearch/binarySearch";
 import selectionSort from "./algorithms/SelectionSort/SelectionSort";
+import sumRecursion from "./algorithms/recursion/sumRecursion";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
-// const arr = [1, 3, 5, 6, 7, 8, 9, 13, 14, 15];
-const arr = [7, 1, 5, 3, 14, 6, 15, 13, 8, 9];
+const arr = [1, 3, 5, 6, 7, 8, 9, 13, 14, 14];
+// const arr = [7, 1, 5, 3, 14, 6, 15, 13, 8, 9];
 
-console.log(selectionSort(arr));
+console.log(sumRecursion(arr));
 
 app.listen(port);
